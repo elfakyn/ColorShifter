b2151b9ceba8f7aab66fb002f0ad40e456052759
elfakyn <elfakyn@gmail.com>
2014-03-09 22:18:56 -0400

    Initial commit
    Changes the theme to a hardcoded color when executed.

---
+++ ColorShifter/dwmParams.h
@@ -0,0 +1,14 @@
+#ifndef DWMPARAMS_H
+#define DWMPARAMS_H
+
+typedef struct { // Color
+	INT colorValue;
+	INT glowValue;
+	INT colorSaturation;
+	INT glowSaturation;
+	INT blurSaturation;
+	INT reflectionIntensity;
+	INT opaqueBlend;
+} DwmColorizationParameters;
+
+#endif
---
+++ ColorShifter/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Windows.h>
+#include "dwmParams.h"
+
+int main()
+{
+	HMODULE hlib = LoadLibrary("dwmapi.dll"); // load the Window Manager API
+
+	if (!hlib) {
+		std::cerr << "Could not load dwmapi.dll" << std::endl;
+		return -1;
+	}
+
+	typedef VOID(*DwmSetColorizationParametersProc) (DwmColorizationParameters *);
+	typedef VOID(*DwmGetColorizationParametersProc) (DwmColorizationParameters *);
+
+	DwmGetColorizationParametersProc DwmGetColorizationParameters =
+		(DwmGetColorizationParametersProc)GetProcAddress(hlib, (LPCSTR)127); // Microsoft calls it DwmGetColorizationParameters
+	if (DwmGetColorizationParameters == NULL) {
+		std::cerr << "Could not load DwmGetColorizationParameters" << std::endl;
+	}
+	
+	DwmSetColorizationParametersProc DwmSetColorizationParameters =
+		(DwmSetColorizationParametersProc)GetProcAddress(hlib, (LPCSTR)131); // Microsoft calls it DwmSetColorizationParameters
+	if (DwmSetColorizationParameters == NULL) {
+		std::cerr << "Could not load DwmSetColorizationParameters" << std::endl;
+	}
+
+	DwmColorizationParameters currentColors = { 0xffff0000, 0, 30, 0, 0, 0, 0 };
+	DwmSetColorizationParameters(&currentColors);
+
+	return 0;
+}

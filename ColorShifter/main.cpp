c738bf560aa3c2c3dce0383a49ef38d828bf4bd9
elfakyn <elfakyn@gmail.com>
2014-03-13 19:23:50 -0400

    IT WORKS!!!
    Wow!!!! I am as surprised as you are!

--- ColorShifter/main.cpp
+++ ColorShifter/main.cpp
@@ -2,39 +2,38 @@
 #include <Windows.h>
 #include "colorTools.h"
 #include "transitionTools.h"
-
+HMODULE hlib;
 int main()
 {
-	HMODULE hlib = LoadLibrary("dwmapi.dll"); // load the Window Manager API
+	hlib = LoadLibrary("dwmapi.dll"); // load the Window Manager API
 
 	if (!hlib) {
 		std::cerr << "Could not load dwmapi.dll" << std::endl;
 		return -1;
 	}
-
-	typedef void(*DwmSetColorizationParametersProc) (DwmColorizationParameters *);
-	typedef void(*DwmGetColorizationParametersProc) (DwmColorizationParameters *);
-
-	DwmGetColorizationParametersProc DwmGetColorizationParameters =
-		(DwmGetColorizationParametersProc)GetProcAddress(hlib, (LPCSTR)127); // Microsoft calls it DwmGetColorizationParameters
-	if (DwmGetColorizationParameters == NULL) {
-		std::cerr << "Could not load DwmGetColorizationParameters" << std::endl;
-	}
-	
-	DwmSetColorizationParametersProc DwmSetColorizationParameters =
-		(DwmSetColorizationParametersProc)GetProcAddress(hlib, (LPCSTR)131); // Microsoft calls it DwmSetColorizationParameters
-	if (DwmSetColorizationParameters == NULL) {
-		std::cerr << "Could not load DwmSetColorizationParameters" << std::endl;
-	}
+	*(FARPROC *)&DwmGetColorizationParameters = GetProcAddress(hlib, (LPCSTR)127);
+	*(FARPROC *)&DwmSetColorizationParameters = GetProcAddress(hlib, (LPCSTR)131);
 
 	Color color1, color2;
 	color1.SetMerged(0xFFFF0000);
 	color1.SetBalance(50);
 	color2.SetMerged(0xFF0000FF);
 	color2.SetBalance(50);
+	int steps = 100;
+	int wait_ms = 30;
+	DwmColorizationParameters crt = { 0 };
+	while (1) {
+		for (int i = 0; i < steps; i++) {
+			crt = exportColor(interpolate(color1, color2, i * 1.0 / steps));
+			DwmSetColorizationParameters(&crt, 0);
+			Sleep(wait_ms);
+		}
+		for (int i = 0; i < steps; i++) {
+			crt = exportColor(interpolate(color2, color1, i * 1.0 / steps));
+			DwmSetColorizationParameters(&crt, 0);
+			Sleep(wait_ms);
+		}
+	}
 
-	singleTransition(color1, color2, 100, 500, DwmSetColorizationParameters);
-	int x;
-	std::cin >> x;
-	return 0;
+//	singleTransition(color1, color2, 100, 500, DwmSetColorizationParameters);
 }
--- ColorShifter/transitionTools.h
+++ ColorShifter/transitionTools.h
@@ -1,9 +1,15 @@
 #ifndef DWM_COLORIZATION_PARAMETERS_PROC
 #define DWM_COLORIZATION_PARAMETERS_PROC
 
-typedef void(*DwmSetColorizationParametersProc) (DwmColorizationParameters *);
-typedef void(*DwmGetColorizationParametersProc) (DwmColorizationParameters *);
+/*
+typedef HRESULT(WINAPI*DwmSetColorizationParametersProc) (DwmColorizationParameters *, UINT);
+typedef HRESULT(WINAPI*DwmGetColorizationParametersProc) (DwmColorizationParameters *);
+*/
+
+HRESULT(WINAPI *DwmSetColorizationParameters) (DwmColorizationParameters *color, UINT unknown);
+HRESULT(WINAPI *DwmGetColorizationParameters) (DwmColorizationParameters *color);
+
 
 #endif
 
-void singleTransition(Color, Color, int, int, DwmSetColorizationParametersProc);
+//void singleTransition(Color, Color, int, int, DwmSetColorizationParametersProc);

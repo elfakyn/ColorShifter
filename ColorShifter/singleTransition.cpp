4a84d07e73f6ed89fac5d0095e36eceb7a06f5cf
elfakyn <elfakyn@gmail.com>
2014-03-13 19:24:48 -0400

    forgot a file

--- ColorShifter/singleTransition.cpp
+++ ColorShifter/singleTransition.cpp
@@ -1,4 +1,4 @@
-#include "Windows.h"
+/*#include "Windows.h"
 #include "colorTools.h"
 #include "transitionTools.h"
 #include <iostream>
@@ -8,7 +8,8 @@ void singleTransition(Color color1, Color color2, int steps, int wait_ms, DwmSet
 	DwmColorizationParameters crt = { 0 };
 	for (int i = 0; i < steps; i++) {
 		crt = exportColor(interpolate(color1, color2, i * 1.0 / steps));
-		setColor(&crt);
+		setColor(&crt, 0);
 		Sleep(wait_ms);
 	}
 }
+*/

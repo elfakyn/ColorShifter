e04b8dd375b67c8dd7b37f0952f08861083f1656
elfakyn <elfakyn@gmail.com>
2014-03-12 21:39:02 -0400

    bah
    I can't git

--- ColorShifter/transitionTools.h
+++ ColorShifter/transitionTools.h
@@ -6,9 +6,4 @@ typedef void(*DwmGetColorizationParametersProc) (DwmColorizationParameters *);
 
 #endif
 
-<<<<<<< HEAD
 void singleTransition(Color, Color, int, int, DwmSetColorizationParametersProc);
-=======
-void singleTransition(Color, Color, int, int, DwmSetColorizationParametersProc);
-
->>>>>>> 4e3c186adc56e2568a1af4d2b982769c4b0839ef

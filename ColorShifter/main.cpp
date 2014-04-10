52fd565b725b56d40b38e1e897b526f432833511
elfakyn <elfakyn@gmail.com>
2014-04-10 15:46:11 -0400

    read color values from file
    The program now reads color values from a file using the following
    format:
    
    <initial color>
    <color 2> <steps to color 2> <wait between steps>
    <color 3> <steps to color 3> <wait between steps>
    ...
    
    and quits when it reaches end of file.
    
    Thanks to Alan O'Donnell ( https://github.com/happy4crazy ) for pairing
    with me on this feature!

--- ColorShifter/main.cpp
+++ ColorShifter/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <Windows.h>
 #include "colorTools.h"
 #include "transitionTools.h"
+
 HMODULE hlib;
 int main()
 {
@@ -14,26 +16,33 @@ int main()
 	*(FARPROC *)&DwmGetColorizationParameters = GetProcAddress(hlib, (LPCSTR)127);
 	*(FARPROC *)&DwmSetColorizationParameters = GetProcAddress(hlib, (LPCSTR)131);
 
+	std::ifstream readColor;
+	readColor.open("colorscheme.txt");
+
 	Color color1, color2;
-	color1.SetMerged(0xFFFF0000);
-	color1.SetBalance(50);
-	color2.SetMerged(0xFF0000FF);
-	color2.SetBalance(50);
-	int steps = 100;
-	int wait_ms = 30;
+	unsigned int steps, wait_ms, currentColorValue;
 	DwmColorizationParameters crt = { 0 };
-	while (1) {
+
+	readColor >> std::hex >> currentColorValue;
+	std::cout << std::hex << currentColorValue << std::endl;
+	color1.SetMerged(currentColorValue);
+	crt = exportColor(color1);
+	DwmSetColorizationParameters(&crt, 0);
+
+
+	while (!readColor.eof()) {
+		readColor >> std::hex >> currentColorValue >> std::dec >> steps >> wait_ms;
+		color2.SetMerged(currentColorValue);
 		for (int i = 0; i < steps; i++) {
 			crt = exportColor(interpolate(color1, color2, i * 1.0 / steps));
 			DwmSetColorizationParameters(&crt, 0);
 			Sleep(wait_ms);
 		}
-		for (int i = 0; i < steps; i++) {
-			crt = exportColor(interpolate(color2, color1, i * 1.0 / steps));
-			DwmSetColorizationParameters(&crt, 0);
-			Sleep(wait_ms);
-		}
+
+		color1 = color2;
 	}
+
+	return 0;
 
 //	singleTransition(color1, color2, 100, 500, DwmSetColorizationParameters);
 }
